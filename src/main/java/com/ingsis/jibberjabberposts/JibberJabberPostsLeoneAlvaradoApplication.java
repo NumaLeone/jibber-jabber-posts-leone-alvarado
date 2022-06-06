a9000d95ea0787c4bf3c8f77c9f449108df5acaf
Numa Leone Elizalde <43762917+NumaLeone@users.noreply.github.com>
2022-06-06 18:07:38 -0300

    Add files via upload

---
+++ src/main/java/com/ingsis/jibberjabberposts/JibberJabberPostsLeoneAlvaradoApplication.java
@@ -0,0 +1,13 @@
+package com.ingsis.jibberjabberposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibberJabberPostsLeoneAlvaradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibberJabberPostsLeoneAlvaradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingsis/jibberjabberposts/JibberJabberPostsLeoneAlvaradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsis.jibberjabberposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibberJabberPostsLeoneAlvaradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
